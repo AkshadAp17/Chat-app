18e66f67a291b46ce75de3d9373c596b25ce2dd0
AkshadAp17 <148571478+AkshadAp17@users.noreply.github.com>
2024-06-06 20:34:22 +0530

    Update connectToMongoDB.js

--- mern-chat-app-master/backend/db/connectToMongoDB.js
+++ mern-chat-app-master/backend/db/connectToMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect("mongodb+srv://akshad:akshad123@cluster0.h7cksjm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+		await mongoose.connect("MONGO_DB.URL");
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
